cf285c9a0494155b39239b7987e065c5f632f551
rohit3607 <rohitplayer87089@gmail.com>
2025-03-07 11:24:22 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -12,13 +12,13 @@ from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated
 
 from bot import Bot
 from config import ADMINS, FORCE_MSG, START_MSG, CUSTOM_CAPTION, DISABLE_CHANNEL_BUTTON, PROTECT_CONTENT
-from helper_func import subscribed, encode, decode, get_messages
-from database.database import add_user, del_user, full_userbase, present_user
+from helper_func import *
+from database.database import *
 
 
 
 
-@Bot.on_message(filters.command('start') & filters.private & subscribed)
+@Bot.on_message(filters.command('start') & filters.private & subscribed1 & subscribed2)
 async def start_command(client: Client, message: Message):
     id = message.from_user.id
     if not await present_user(id):
