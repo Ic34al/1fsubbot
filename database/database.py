a6db78b0ae3b5a4722ebe17f8d808d057acf19e0
Ic34al <138458465+Ic34al@users.noreply.github.com>
2026-08-20 12:35:09 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -1,36 +1,45 @@
-#(©)CodeXBotz
+from motor.motor_asyncio import AsyncIOMotorClient
 
-
-
-
-import pymongo, os
 from config import DB_URI, DB_NAME
 
 
-dbclient = pymongo.MongoClient(DB_URI)
+dbclient = AsyncIOMotorClient(DB_URI)
+
 database = dbclient[DB_NAME]
 
-
-user_data = database['users']
-
+user_data = database["users"]
 
 
-async def present_user(user_id : int):
-    found = user_data.find_one({'_id': user_id})
-    return bool(found)
+async def present_user(user_id: int):
+    found = await user_data.find_one(
+        {"_id": int(user_id)},
+        {"_id": 1}
+    )
+
+    return found is not None
+
 
 async def add_user(user_id: int):
-    user_data.insert_one({'_id': user_id})
-    return
+    await user_data.update_one(
+        {"_id": int(user_id)},
+        {"$setOnInsert": {"_id": int(user_id)}},
+        upsert=True
+    )
+
 
 async def full_userbase():
-    user_docs = user_data.find()
-    user_ids = []
-    for doc in user_docs:
-        user_ids.append(doc['_id'])
-        
-    return user_ids
+    cursor = user_data.find(
+        {},
+        {"_id": 1}
+    )
+
+    return [
+        document["_id"]
+        async for document in cursor
+    ]
+
 
 async def del_user(user_id: int):
-    user_data.delete_one({'_id': user_id})
-    return
+    await user_data.delete_one(
+        {"_id": int(user_id)}
+    )
