3b2d7b031356aa214e3dc554cc0f239c5babcd00
Ic34al <138458465+Ic34al@users.noreply.github.com>
2026-08-20 13:18:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-import asyncio
 from bot import Bot
-import pyrogram.utils
-
-# Fix Heroku event loop issue
-loop = asyncio.new_event_loop()
-asyncio.set_event_loop(loop)
-
-pyrogram.utils.MIN_CHANNEL_ID = -1009147483647
 
 Bot().run()
