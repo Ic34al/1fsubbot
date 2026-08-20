3b871693c26b9bf460f5034a17a5a1f3e31b3d9a
Ic34al <138458465+Ic34al@users.noreply.github.com>
2026-08-20 14:16:39 +0530

    Update useless.py

--- plugins/useless.py
+++ plugins/useless.py
@@ -5,7 +5,8 @@ from config import ADMINS, BOT_STATS_TEXT, USER_REPLY_TEXT
 from datetime import datetime
 from helper_func import get_readable_time
 
-@Bot.on_message(filters.command('stats') & filters.user(ADMINS))
+
+@Bot.on_message(filters.command("stats") & filters.user(ADMINS))
 async def stats(bot: Bot, message: Message):
     now = datetime.now()
     delta = now - bot.uptime
@@ -13,7 +14,12 @@ async def stats(bot: Bot, message: Message):
     await message.reply(BOT_STATS_TEXT.format(uptime=time))
 
 
-@Bot.on_message(filters.private & filters.incoming)
-async def useless(_,message: Message):
+# Reply only to normal messages, never to commands such as /start
+@Bot.on_message(
+    filters.private
+    & filters.incoming
+    & ~filters.command(["start", "genlink", "batch", "stats"])
+)
+async def useless(_, message: Message):
     if USER_REPLY_TEXT:
         await message.reply(USER_REPLY_TEXT)
